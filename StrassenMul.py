72de7f7cf68cf49beac7de609a489444a9205097
Johannes Hölzl <johannes.hoelzl@gmail.com>
2019-12-18 22:06:34 +0100

    Results

--- StrassenMul.py
+++ StrassenMul.py
@@ -1,5 +1,4 @@
 class StrassenMul:
-
     def __init__(self, a, b):
         self.a = a
         self.b = b
@@ -10,8 +9,9 @@ class StrassenMul:
     @staticmethod
     def _split_array(a):
         l = len(a)
-        return [[[a[i][s: e] for i in range(x, y)] for s, e in ((0, l // 2), (l // 2, l))] for x, y in
-                ((0, l // 2), (l // 2, l))]
+        lh = l//2
+        return [[[a[i][s: e] for i in range(x, y)] for s, e in ((0, lh), (lh, l))] for x, y in
+                ((0, lh), (lh, l))]
 
     @staticmethod
     def _matrix_add(a, b):
@@ -26,14 +26,23 @@ class StrassenMul:
 
     @staticmethod
     def mul(a, b):
+        """
+        Multiplication using the Strassen algorithm.
+
+        :param a: first matrix in multiplication
+        :param b: second matrix in multiplication
+        :return:
+        """
 
         w = len(a)
 
-        if w > 2:
+        if w > 2 and w % 2 == 0:
 
+            # split a and b into quarters
             a_split = StrassenMul._split_array(a)
             b_split = StrassenMul._split_array(b)
 
+            # needed in next step
             a11_plus_a22 = StrassenMul._matrix_add(a_split[0][0], a_split[1][1])
             a21_plus_a22 = StrassenMul._matrix_add(a_split[1][0], a_split[1][1])
             a11_plus_a12 = StrassenMul._matrix_add(a_split[0][0], a_split[0][1])
@@ -46,6 +55,7 @@ class StrassenMul:
             b11_plus_b12 = StrassenMul._matrix_add(b_split[0][0], b_split[0][1])
             b21_plus_b22 = StrassenMul._matrix_add(b_split[1][0], b_split[1][1])
 
+            # calculating the
             m1 = StrassenMul.mul(a11_plus_a22, b11_plus_b22)
             m2 = StrassenMul.mul(a21_plus_a22, b_split[0][0])
             m3 = StrassenMul.mul(a_split[0][0], b12_minus_b22)
@@ -54,34 +64,26 @@ class StrassenMul:
             m6 = StrassenMul.mul(a21_minus_a11, b11_plus_b12)
             m7 = StrassenMul.mul(a12_minus_a22, b21_plus_b22)
 
+            # construct quarters of resulting matrix
             c11 = StrassenMul._matrix_add(m1, StrassenMul._matrix_add(m4, StrassenMul._matrix_sub(m7,m5)))
             c12 = StrassenMul._matrix_add(m3, m5)
             c21 = StrassenMul._matrix_add(m2, m4)
             c22 = StrassenMul._matrix_add(m1, StrassenMul._matrix_add(m3, StrassenMul._matrix_sub(m6, m2)))
 
+            # putting smaller matrices together to resulting matrix
             upper = [c11[i] + c12[i] for i in range(len(c11))]
-            lower = [c21[i] + c22[i] for i in range(len(c11))]
+            lower = [c21[i] + c22[i] for i in range(len(c21))]
 
             return upper + lower
 
         else:
-
-            m1 = (a[0][0] + a[1][1]) * (b[0][0] + b[1][1])
-            m2 = (a[1][0] + a[1][1]) * b[0][0]
-            m3 = a[0][0] * (b[0][1] - b[1][1])
-            m4 = a[1][1] * (b[1][0] - b[0][0])
-            m5 = (a[0][0] + a[0][1]) * b[1][1]
-            m6 = (a[1][0] - a[0][0]) * (b[0][0] + b[0][1])
-            m7 = (a[0][1] - a[1][1]) * (b[1][0] + b[1][1])
-
-            c11 = m1 + m4 - m5 + m7
-            c12 = m3 + m5
-            c21 = m2 + m4
-            c22 = m1 - m2 + m3 + m6
-
-            return [[c11, c12], [c21, c22]]
+            return StrassenMul._mul_naive(a, b)
 
     def mul_normal_2x2(self):
+        """
+        Naive for 2x2...
+        :return:
+        """
         oc11 = self.a[0][0] * self.b[0][0] + self.a[0][1] * self.b[1][0]
         oc12 = self.a[0][0] * self.b[0][1] + self.a[0][1] * self.b[1][1]
         oc21 = self.a[1][0] * self.b[0][0] + self.a[1][1] * self.b[1][0]
@@ -89,9 +91,17 @@ class StrassenMul:
 
         print([[oc11, oc12], [oc21, oc22]])
 
-    def mul_normal(self):
+    def mul_naive(self):
+        """
+        Run _mul_naive for a and b of self
+        :return:
         """
 
+        return StrassenMul._mul_naive(self.a, self.b)
+
+    @staticmethod
+    def _mul_naive(a, b):
+        """
         complexity: O(n^3)
 
         ->->->...
@@ -101,14 +111,10 @@ class StrassenMul:
         ->->->...
         ...
 
+        :param a: first matrix in multiplication
+        :param b: second matrix in multiplication
         :return:
         """
-
-        a = self.a
-        b = self.b
-
-        print("a:",a)
-        print("b:",b)
 
         matrix_len = len(a)
 
@@ -130,13 +136,15 @@ class StrassenMul:
         return c
 
 
+# code for trying the functionality:
+
 def small_matrix():
     a = [[1, 2], [3, 4]]
     b = [[5, 6], [7, 8]]
     sm = StrassenMul(a, b)
 
     print("s:", sm.mul_s())
-    print("n:",sm.mul_normal())
+    print("n:", sm.mul_naive())
 
 
 def four_x_four_matrix():
@@ -148,7 +156,7 @@ def four_x_four_matrix():
 
     print("4x4:")
     print("s:",sm.mul_s())
-    print("n:",sm.mul_normal())
+    print("n:", sm.mul_naive())
 
 
 if __name__ == '__main__':
