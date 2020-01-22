8e18041e75337a7482e92f172833cf2727b67fd7
Johannes Hölzl <johannes.hoelzl@gmail.com>
2020-01-22 21:12:04 +0100

    Progress

--- CliqueGreedy.py
+++ CliqueGreedy.py
@@ -8,11 +8,11 @@ class Graph:
     On instantiation the vertices get extracted and the degrees are calculated and stored for each vertex
     """
 
-    def __init__(self, edges):
+    def __init__(self, edges, vs=None):
         self.edges = edges
 
         # calculate vertices from given edges
-        self.vertices = set([e[0] for e in self.edges] + [e[1] for e in self.edges])
+        self.vertices = vs if vs is not None else {[e[0] for e in self.edges] + [e[1] for e in self.edges]}
 
         self.degrees = {vn: 0 for vn in self.vertices}
 
@@ -32,7 +32,7 @@ def get_clique(graph):
     # used vertices
     clique = [vertex for vertex in graph.vertices]
 
-    c_edges = set(edge for edge in graph.edges)
+    c_edges = {edge for edge in graph.edges}
 
     # sort by degrees and only keep vertex names
     sorted_degrees = [x[0] for x in sorted(graph.degrees.items(), key=lambda el: el[1], reverse=True)]
@@ -44,9 +44,14 @@ def get_clique(graph):
 
         # could be optimized...
         inner_edges = [edge for edge in c_edges if edge[0] == v or edge[1] == v]
-        clique = set([x[0] for x in inner_edges] + [x[1] for x in inner_edges]).intersection(clique)
+
+        # make sure that no unwanted vertices are accidentally added
+        # and v has to be included even when there were no edges
+        clique = set([x[0] for x in inner_edges] + [x[1] for x in inner_edges]).intersection(clique).union({v})
         c_edges = [edge for edge in graph.edges if edge[0] in clique or edge[1] in clique]
 
+        # corrections due to apparently wrong algorithm in slides
+        # only terminate when "clique" is actually a clique
         if len(c_edges) == last_size and is_clique(clique, graph):
             break
 
@@ -71,7 +76,6 @@ def is_clique(clique, graph):
     return True
 
 
-
 def gen_random_graph_with_clique(size, clique_size, r):
     """
     Generates graph with a clique of size clique_size containing randomly selected vertices.
@@ -82,26 +86,26 @@ def gen_random_graph_with_clique(size, clique_size, r):
      clique
     :return: Graph object containing a graph with a cliques_size clique
     """
-    v = [v for v in range(1, size + 1)]
+    vertices = [v for v in range(1, size + 1)]
     g_edges = []
 
-    clique = random.sample(v, clique_size)
+    clique = random.sample(vertices, clique_size)
 
-    degrees = {vn: (clique_size if vn in clique else 0) for vn in v}
+    degrees = {vn: (clique_size if vn in clique else 0) for vn in vertices}
 
     # create clique
-    for i in range(0, len(v) - 1):
-        vi = v[i]
-        for j in range(i + 1, len(v)):
-            vj: int = v[j]
+    for i in range(0, len(vertices) - 1):
+        vi = vertices[i]
+        for j in range(i + 1, len(vertices)):
+            vj: int = vertices[j]
             if vi in clique and vj in clique:
                 g_edges.append((vi, vj))
 
     # add other edges
-    for i in range(0, len(v)-1):
-        vi = v[i]
-        for j in range(i+1, len(v)):
-            vj: int = v[j]
+    for i in range(0, len(vertices)-1):
+        vi = vertices[i]
+        for j in range(i+1, len(vertices)):
+            vj: int = vertices[j]
 
             if r >= random.random() \
                     and (degrees[vi] < clique_size - 1 or degrees[vj] < clique_size - 1) \
@@ -110,4 +114,4 @@ def gen_random_graph_with_clique(size, clique_size, r):
                 degrees[vi] += 1
                 degrees[vj] += 1
 
-    return Graph(g_edges)
+    return Graph(g_edges, set(vertices))
